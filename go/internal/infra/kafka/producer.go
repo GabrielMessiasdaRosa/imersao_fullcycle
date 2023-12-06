b2f4d033e38412006819810c4c244c3e9a5e2212
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-12-06 14:59:15 -0300

    Add Kafka producer implementation

---
+++ go/internal/infra/kafka/producer.go
@@ -0,0 +1,35 @@
+package kafka
+
+import (
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+type Producer struct {
+	ConfigMap *ckafka.ConfigMap
+}
+
+func NewKafkaProducer(configMap *ckafka.ConfigMap) *Producer {
+	return &Producer{
+		ConfigMap: configMap,
+	}
+}
+
+func (producer *Producer) Publish(msg string, key []byte, topic string) error {
+	p, err := ckafka.NewProducer(producer.ConfigMap)
+	if err != nil {
+		return err
+	}
+	message := &ckafka.Message{
+		TopicPartition: ckafka.TopicPartition{
+			Topic:     &topic,
+			Partition: ckafka.PartitionAny,
+		},
+		Key:   key,
+		Value: []byte(msg),
+	}
+	err = p.Produce(message, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
